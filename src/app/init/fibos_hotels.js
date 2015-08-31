125dea637776d81013733edfc876a635bf8c6c16
Fabio Di Monte <fdimonte@expedia.com>
2015-08-31 13:21:23 +0200

    improved spacers colors for hcom

--- src/app/init/fibos_hotels.js
+++ src/app/init/fibos_hotels.js
@@ -5,10 +5,9 @@
 var hotelsOptions = {
     logEvents: false,
     uiSpacer:  {
-        spacerMin:1,
         spacerSymbols : [{s:'♠︎',f:1.4,l:0.9},{s:'♣',f:1.4,l:0.9},{s:'♥',f:1.4,l:0.9},{s:'♦︎',f:1.4,l:0.9}],
-        spacersList:[4,8,12,16,20,24,28,32,36,40,44,48,52,56,60],
-        spacerColors  : ['#0071bc','#ed1e79','#8cc63f']
+        spacersList:[1,2,4,8,12,16,20,24,28,32,36,40,44,48,52,56,60,64,68,72],
+        spacerColors  : ['#0071bc','#ed1e79','#8cc63f','#fbb03b']
     },
     uiMarker:  {
         taglist:{dt:true,dd:true}
